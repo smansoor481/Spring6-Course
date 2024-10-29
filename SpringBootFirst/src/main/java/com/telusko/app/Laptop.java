4fcb59714c9d5db80fea336ddba081c09bb19068
= <mansoorshaik2621@gmail.com>
2024-10-29 15:04:51 +0530

    Autowired in springBoot

---
+++ SpringBootFirst/src/main/java/com/telusko/app/Laptop.java
@@ -0,0 +1,13 @@
+package com.telusko.app;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Laptop {
+	
+	public void compile()
+	{
+		System.out.println("Compiling...");
+	}
+
+}
--- SpringBootFirst/src/main/java/com/telusko/app/SpringBootFirstApplication.java
+++ SpringBootFirst/src/main/java/com/telusko/app/SpringBootFirstApplication.java
@@ -2,13 +2,17 @@ package com.telusko.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringBootFirstApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootFirstApplication.class, args);
-		System.out.println("Hello world");
+		ApplicationContext context = SpringApplication.run(SpringBootFirstApplication.class, args);
+		Alien obj = context.getBean(Alien.class);
+		obj.code();
+		
+		
 	}
 
 }
