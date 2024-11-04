90fc75688a3c7f1bad4338f6eba017dccf1bb741
Mansoor Shaik <133763110+smansoor481@users.noreply.github.com>
2024-11-04 17:32:36 +0530

    Setter Injection & ref Attribute
    Setter injection
    
    	- Let suppose if we want to add setters and getters in the alien class, 
    	- We want setage and getage , call this one..
    	- So we want to call this and add it in xml like DepencyInjection we want to add i.e. 
    		○ <Property/> tag
    		○ Inside this we want to add what you want
    		○ <Property name="age" value="21"/>
    			§ For primitive we have add value name..
    Setter injection
    	- Now for primitive type we use value / and references/obj are we use ref..
    	- We have create one more class laptop have an method, and add it in a alien class, and create an laptop object.
    	- executed so but we want spring create an object so in xml add one more property and in the ref add it in a laptop ref(spring create an object)
    What type of ref you are given that bean has executed….

---
+++ Spring/setter injection & ref attribute/src/main/java/com/telusko/Spring/Laptop.java
@@ -0,0 +1,13 @@
+package com.telusko.Spring;
+
+public class Laptop {
+	public   Laptop()
+	{
+		System.out.println("Laptop object created...");
+	}
+	public void  compile()
+	{
+		System.out.println("Compiling");
+	}
+
+}
