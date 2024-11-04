861f852bd8c2172d5d9e82836fe2831540acb570
Mansoor Shaik <133763110+smansoor481@users.noreply.github.com>
2024-11-04 17:09:37 +0530

    Scopes in Spring
    	- In the bean we have scopes,
    		○ They are 4 types , we can use 2 in spring core
    			§ Singleton
    			§ Prototype
    		○ For singleton same output will come, if we can pass one object, it also has a same name for different objects.
    		○ By prototype , every time getBean specially want to create an object. Suppose you created 2 times getBean, 2 objects will create, by using getBean..
    By default create an object for singleton, but prototype is when you call the getBean is then only create an objects…

---
+++ scopes/src/main/java/com/telusko/Spring/Alien.java
@@ -0,0 +1,17 @@
+package com.telusko.Spring;
+
+public class Alien {
+	int age;
+	public Alien()
+	{
+		System.out.println("Object created");
+	}
+	 
+	 
+	public void code()
+	{
+		System.out.println("Coding..");
+	}
+	
+
+}
---
+++ scopes/src/main/java/com/telusko/Spring/Laptop.java
@@ -0,0 +1,9 @@
+package com.telusko.Spring;
+
+public class Laptop {
+	public   Laptop()
+	{
+		System.out.println("Laptop object created...");
+	}
+
+}
