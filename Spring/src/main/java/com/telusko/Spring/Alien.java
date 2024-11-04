38189c0b0a9e603f9b736f20256cc3466e808ec2
Mansoor Shaik <133763110+smansoor481@users.noreply.github.com>
2024-11-04 16:51:27 +0530

    Object Creation
    Object creation of the spring, how many beans are created , that many objects are created

---
+++ Spring/src/main/java/com/telusko/Spring/Alien.java
@@ -0,0 +1,17 @@
+package com.telusko.Spring;
+
+public class Alien {
+	
+	public Alien()
+	{
+		System.out.println("Object created");
+	}
+	 
+	 
+	public void code()
+	{
+		System.out.println("Coding..");
+	}
+	
+
+}
---
+++ Spring/src/main/java/com/telusko/Spring/Laptop.java
@@ -0,0 +1,12 @@
+package com.telusko.Spring;
+
+import org.springframework.stereotype.Component;
+
+public class Laptop {
+	
+	public void compile()
+	{
+		System.out.println("Compiling...");
+	}
+
+}
